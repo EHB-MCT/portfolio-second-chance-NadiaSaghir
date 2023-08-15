0b557415b9698104af63d58c280e07e7745e0841
NadiaSaghir <nadia.saghir@student.ehb.be>
2023-08-16 00:28:35 +0200

    feat docker: Add docker-compose file
    Add docker-compose file and setup for API and PostgreSQL

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const app = express();
 
 app.get("/", (request, response) =>{
-    response.send({message: "hi world"})
+    response.send({message: "Hey world"})
 })
 
 app.listen(3000, (err) => {
