d883fa1d69d43fac268e185a50b71304ed6b3945
NadiaSaghir <nadia.saghir@student.ehb.be>
2023-08-15 22:27:36 +0200

    initial commit express & nodemon

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+const app = express();
+
+app.get("/", (request, response) =>{
+    response.send({message: "hi world"})
+})
+
+app.listen(3000, (err) => {
+    if (!err) {
+        console.log("runnung on port " + 3000);
+    }
+    else {
+        console.error(err)
+    }
+})
