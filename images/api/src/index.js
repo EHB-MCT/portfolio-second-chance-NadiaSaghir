84f544fc90571a007ebf25e77fb385ff5d771bf0
NadiaSaghir <nadia.saghir@student.ehb.be>
2023-08-18 18:36:02 +0200

    feat/recipe-endpoint: Set up recipe database and schema

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -1,10 +1,13 @@
 const express = require ("express");
 const knex = require('knex');
+const crypto = require('crypto');
 
 const app = express();
 const port = 3000;
 
-// Create a Knex instance with PostgreSQL as the database driver
+app.use(express.json());
+
+// Knex instance with PostgreSQL as the database driver
 const db = knex({
     client: 'pg',
     connection: {
@@ -14,8 +17,8 @@ const db = knex({
       database: 'test'
     }
   });
-  
-// Create the user table if it doesn't exist
+
+// Create user table if doesn't exist
 db.schema
 .hasTable('users')
 .then((exists) => {
@@ -24,29 +27,82 @@ db.schema
       table.increments('id').primary();
       table.string('name');
       table.string('email').unique();
+      table.string('password');
     });
   }
 })
   .then(() => console.log('User table created'))
   .catch((error) => console.error(error));
 
-app.use(express.json()); // Enable JSON parsing for incoming requests
+// Create user table if doesn't exist
+db.schema
+  .hasTable('recipes')
+  .then((exists) => {
+    if (!exists) {
+      return db.schema.createTable('recipes', function (table) {
+        table.increments('id').primary();
+        table.string('title');
+        table.text('ingredients');
+        table.text('preparation');
+        table.integer('servings');
+        table.string('image');
+      });
+    }
+  })
+  .then(() => console.log('Recipes table created'))
+  .catch((error) => console.error(error));
 
-// Define a route to add a new user
-app.post('/users', async (req, res) => {
-  const { name, email } = req.body;
 
-  try {
-    // Insert the new user into the database
-    await db('users').insert({ name, email });
+// POST Sign Up
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    try {
+      // Create a hash
+      const hash = crypto.createHash('sha256');
+  
+      // Hash password
+      const hashedPassword = hash.update(password).digest('hex');
+  
+      // Insert new user to users table with hashed password
+      await db('users').insert({ name, email, password: hashedPassword });
+  
+      res.status(201).json({ message: 'User registered successfully.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while registering the user.' });
+    }
+  });
+  
+  
+// POST login
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Check if user with this email exists
+      const user = await db('users').where('email', email).first();
+  
+      if (!user) {
+        res.status(404).json({ error: 'User not found.' });
+      } else {
+        const hash = crypto.createHash('sha256');
+        const hashedInputPassword = hash.update(password).digest('hex');
+  
+        // Compare input password with hashed password stored in users table
+        if (hashedInputPassword === user.password) {
+          res.json({ message: 'User logged in successfully.' });
+        } else {
+          res.status(401).json({ error: 'Incorrect password.' });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while logging in.' });
+    }
+  });
 
-    res.status(201).json({ message: 'User created successfully.' });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while creating the user.' });
-  }
-});
-
-// Define a route to retrieve all users
+// GET all users, example: http://localhost:80/users
 app.get('/users', async (req, res) => {
   try {
     const users = await db.select().from('users');
@@ -57,12 +113,39 @@ app.get('/users', async (req, res) => {
 });
 
 
-// New route for the root page
-app.get("/", (req, res) => {
-    res.send({ message: "Hey world" });
-});
+// DELETE all users, example: http://localhost:80/users
+app.delete('/users', async (req, res) => {
+    try {
+      // Delete all users
+      await db('users').del();
+      
+      res.json({ message: 'All users deleted successfully.' });
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while deleting users.' });
+    }
+  });
+  
 
-app.listen(port, (err) => {
+// DELETE a specific user by ID, example: http://localhost:80/users/8
+app.delete('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+      // Delete user with specified ID from database
+      const deletedCount = await db('users').where('id', userId).del();
+      
+      if (deletedCount === 0) {
+        res.status(404).json({ error: 'User not found.' });
+      } else {
+        res.json({ message: 'User deleted successfully.' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred while deleting the user.' });
+    }
+  });
+  
+
+  app.listen(port, (err) => {
     if (!err) {
         console.log(`Server is running on port ${port}`);
     }
