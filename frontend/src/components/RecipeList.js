638307c5392a347480e353f29145211f6e83f91e
NadiaSaghir <nadia.saghir@student.ehb.be>
2023-08-20 01:33:17 +0200

    feat/dockerize-react: Update API endpoint for recipe data in RecipeList component

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -5,7 +5,7 @@ function RecipeList() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:3000/recipes')
+    fetch('http://localhost:80/recipes')
       .then((response) => response.json())
       .then((data) => {
         setRecipes(data);
